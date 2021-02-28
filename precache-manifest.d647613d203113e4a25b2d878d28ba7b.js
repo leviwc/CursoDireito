e61ec9d3bc9892fa651edde56d93610000e08399
leviwc <leviwc3@hotmail.com>
2021-02-27 22:27:27 -0300

    Updates

---
+++ precache-manifest.d647613d203113e4a25b2d878d28ba7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "778ee4f89e7b14fddc0753593ded0cc8",
+    "url": "/CursoDireito/index.html"
+  },
+  {
+    "revision": "78791632aec022cd7576",
+    "url": "/CursoDireito/static/css/main.6e722a10.chunk.css"
+  },
+  {
+    "revision": "161f2c34e09e154dbe4c",
+    "url": "/CursoDireito/static/js/2.f9071a03.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/CursoDireito/static/js/2.f9071a03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78791632aec022cd7576",
+    "url": "/CursoDireito/static/js/main.e99f8363.chunk.js"
+  },
+  {
+    "revision": "973e5fe7c1b7e43fd710",
+    "url": "/CursoDireito/static/js/runtime-main.b40518fa.js"
+  }
+]);
